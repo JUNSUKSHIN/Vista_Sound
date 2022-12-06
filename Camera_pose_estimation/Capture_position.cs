991d088d49b4f534631f866f7bf2b838fa0a2d20
JUNSUKSHIN <junsuk2017@chungbuk.ac.kr>
2022-12-07 04:41:24 +0900

    create script for position recording

---
+++ Camera_pose_estimation/Capture_position.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Capture_position : MonoBehaviour
+{
+
+    public GameObject Camera_object;
+    public GameObject Marker_object;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
