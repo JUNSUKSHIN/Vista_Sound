d0f1590e2f9bfa5fb16cb53e1091a6c1ac7fc555
JUNSUKSHIN <junsuk2017@chungbuk.ac.kr>
2022-12-07 08:37:54 +0900

    syns image to axis time

--- Video_processing/image_combiner.py
+++ Video_processing/image_combiner.py
@@ -3,7 +3,27 @@ import glob
 
 img_path = "./image"
 vid_path = "./video"
-files = glob.glob(img_path + '/*')
+img_files = glob.glob(img_path + '/*')
+vid_file = glob.glob(vid_path + '/*')
 
-for f in files:
-    os.rename(f, os.path.join(path, 'img_' + os.path.basename(f)))
+for f in img_files:
+    for j in vid_file:
+        hh = "00"
+        if int(int(os.path.basename(j)[9:11]) + int(int(os.path.basename(f)[7:11])/3600)) < 10:
+            hh = "0" + str(int(os.path.basename(j)[9:11]) + int(int(os.path.basename(f)[7:11])/3600))
+        else:
+            hh = str(int(os.path.basename(j)[9:11]) + int(int(os.path.basename(f)[7:11])/3600))
+
+        mm = "00"
+        if int(int(os.path.basename(j)[11:13]) + int(int(os.path.basename(f)[7:11])/60)) < 10:
+            mm = "0" + str(int(os.path.basename(j)[11:13]) + int(int(os.path.basename(f)[7:11])/60))
+        else:
+            mm = str(int(os.path.basename(j)[11:13]) + int(int(os.path.basename(f)[7:11])/60))
+        ss = "00"
+
+        if int(int(os.path.basename(j)[13:15]) + int(os.path.basename(f)[7:11]) - 1) < 10:
+            ss = "0" + str(int(os.path.basename(j)[13:15]) + int(os.path.basename(f)[7:11]) - 1)
+        else:
+            ss = str(int(os.path.basename(j)[13:15]) + int(os.path.basename(f)[7:11]) - 1)
+
+        os.rename(f, os.path.join(img_path,os.path.basename(j)[0:8] + "_" + hh + mm + ss +".png"))
