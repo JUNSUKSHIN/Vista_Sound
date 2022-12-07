55decdd995feb01f0c713cb4256f02f7a6ba36c0
JUNSUKSHIN <junsuk2017@chungbuk.ac.kr>
2022-12-08 02:05:39 +0900

    separate points from vector

--- Video_processing/Marker_masking.py
+++ Video_processing/Marker_masking.py
@@ -7,10 +7,19 @@ img_files = glob.glob(img_path + '/*')
 
 for img in img_files:
 
-    im = cv2.imread(img)
+    im = cv2.imread("20221208_001741.jpg")
 
     arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_50)
     arucoParams = cv2.aruco.DetectorParameters_create()
     (corners, ids, rejected) = cv2.aruco.detectMarkers(im, arucoDict, parameters=arucoParams)
 
     print(corners)
+
+    point_1, point_2, point_3, point_4 = str(corners).split("],")
+    x1, y1 = point_1.split(".,")
+    x2, y2 = point_2.split(".,")
+    x3, y3 = point_3.split(".,")
+    x4, y4 = point_4.split(".,")
+
+
+
