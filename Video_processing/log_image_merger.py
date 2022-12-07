03e27e831bc8c8992f0da93d7fd9b0bec2ca1ca9
JUNSUKSHIN <junsuk2017@chungbuk.ac.kr>
2022-12-07 16:31:03 +0900

    split date and remove except numbers

--- Video_processing/log_image_merger.py
+++ Video_processing/log_image_merger.py
@@ -1,7 +1,19 @@
 import os
 import glob
+import re
 
 img_path = "./image"
 
 img_files = glob.glob(img_path + '/*')
 f= open('matrix_quotes.txt','r')
+
+lines = f.read().splitlines()
+
+for line in lines:
+    time_t, camera, marker = line.split("/")
+    time_d, time_h = time_t.split(" ")
+
+    time_dn = re.sub(r'[^0-9]', '', time_d)
+    time_hn = re.sub(r'[^0-9]', '', time_h)
+
+
