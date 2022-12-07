a293a48b24e3dfe01b426d3d07be7f9b780eeafe
JUNSUKSHIN <junsuk2017@chungbuk.ac.kr>
2022-12-07 23:33:01 +0900

    batch process for images

--- Video_processing/Marker_masking.py
+++ Video_processing/Marker_masking.py
@@ -1,5 +1,13 @@
 import cv2
+import glob
 
-arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_50)
-arucoParams = cv2.aruco.DetectorParameters_create()
-(corners, ids, rejected) = cv2.aruco.detectMarkers(image, arucoDict, parameters=arucoParams)
+img_path = "./merged_img"
+img_files = glob.glob(img_path + '/*')
+
+for img in img_files:
+
+    arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_50)
+    arucoParams = cv2.aruco.DetectorParameters_create()
+    (corners, ids, rejected) = cv2.aruco.detectMarkers(img, arucoDict, parameters=arucoParams)
+
+    print(corners)
