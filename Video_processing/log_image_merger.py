466c55184a1e2fe413ba2637458a066483f639dc
JUNSUKSHIN <junsuk2017@chungbuk.ac.kr>
2022-12-07 17:08:33 +0900

    convert to relative coordinates

--- Video_processing/log_image_merger.py
+++ Video_processing/log_image_merger.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import re
+import shutil
 
 img_path = "./image"
 
@@ -21,7 +22,14 @@ for line in lines:
     for img in img_files:
 
         if time_m == os.path.basename(img)[0:-4]:
+
+            ca_x, ca_y, ca_z = map(float, camera.split(", "))
+            ma_x, ma_y, ma_z = map(float, marker.split(", "))
             
+            fix_x = ca_x - ma_x
+            fix_y = ca_y - ma_y
+            fix_z = ca_z - ma_z
+
 
 
 
