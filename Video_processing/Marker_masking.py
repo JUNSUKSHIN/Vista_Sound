f1031fd897d98226e5829622f38593157a7c1aae
JUNSUKSHIN <junsuk2017@chungbuk.ac.kr>
2022-12-08 00:41:41 +0900

    aruco marker settings

--- Video_processing/Marker_masking.py
+++ Video_processing/Marker_masking.py
@@ -1,13 +1,16 @@
 import cv2
 import glob
+import numpy as np
 
 img_path = "./merged_img"
 img_files = glob.glob(img_path + '/*')
 
 for img in img_files:
 
-    arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_50)
+    im = cv2.imread(img)
+
+    arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_50)
     arucoParams = cv2.aruco.DetectorParameters_create()
-    (corners, ids, rejected) = cv2.aruco.detectMarkers(img, arucoDict, parameters=arucoParams)
+    (corners, ids, rejected) = cv2.aruco.detectMarkers(im, arucoDict, parameters=arucoParams)
 
     print(corners)
