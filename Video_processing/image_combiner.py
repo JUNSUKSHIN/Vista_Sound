56a1abc7203502fc418ac162eb5dc5c03b72dafc
JUNSUKSHIN <junsuk2017@chungbuk.ac.kr>
2022-12-07 18:30:43 +0900

    added some lines for am, pm support

--- Video_processing/image_combiner.py
+++ Video_processing/image_combiner.py
@@ -9,21 +9,21 @@ vid_file = glob.glob(vid_path + '/*')
 for f in img_files:
     for j in vid_file:
         hh = "00"
-        if int(int(os.path.basename(j)[9:11]) + int(int(os.path.basename(f)[7:11])/3600)) < 10:
-            hh = "0" + str(int(os.path.basename(j)[9:11]) + int(int(os.path.basename(f)[7:11])/3600))
+        if int(int(os.path.basename(j)[9:11]) + int(int(os.path.basename(f)[3:7])/3600)) < 10:
+            hh = "0" + str(int(os.path.basename(j)[9:11]) + int(int(os.path.basename(f)[3:7])/3600))
         else:
-            hh = str(int(os.path.basename(j)[9:11]) + int(int(os.path.basename(f)[7:11])/3600))
+            hh = str(int(os.path.basename(j)[9:11]) + int(int(os.path.basename(f)[3:7])/3600))
 
         mm = "00"
-        if int(int(os.path.basename(j)[11:13]) + int(int(os.path.basename(f)[7:11])/60)) < 10:
-            mm = "0" + str(int(os.path.basename(j)[11:13]) + int(int(os.path.basename(f)[7:11])/60))
+        if int(int(os.path.basename(j)[11:13]) + int(int(os.path.basename(f)[3:7])/60)) < 10:
+            mm = "0" + str(int(os.path.basename(j)[11:13]) + int(int(os.path.basename(f)[3:7])/60))
         else:
-            mm = str(int(os.path.basename(j)[11:13]) + int(int(os.path.basename(f)[7:11])/60))
+            mm = str(int(os.path.basename(j)[11:13]) + int(int(os.path.basename(f)[3:7])/60))
         ss = "00"
 
-        if int(int(os.path.basename(j)[13:15]) + int(os.path.basename(f)[7:11]) - 1) < 10:
-            ss = "0" + str(int(os.path.basename(j)[13:15]) + int(os.path.basename(f)[7:11]) - 1)
+        if int(int(os.path.basename(j)[13:15]) + int(os.path.basename(f)[3:7]) - 1) < 10:
+            ss = "0" + str(int(os.path.basename(j)[13:15]) + int(os.path.basename(f)[3:7]) - 1)
         else:
-            ss = str(int(os.path.basename(j)[13:15]) + int(os.path.basename(f)[7:11]) - 1)
+            ss = str(int(os.path.basename(j)[13:15]) + (int(os.path.basename(f)[3:7]) % 60) - 1)
 
         os.rename(f, os.path.join(img_path,os.path.basename(j)[0:8] + "_" + hh + mm + ss +".png"))
--- Video_processing/log_image_merger.py
+++ Video_processing/log_image_merger.py
@@ -2,6 +2,7 @@ import os
 import glob
 import re
 import shutil
+import math
 
 img_path = "./image"
 
@@ -18,18 +19,26 @@ for line in lines:
     time_hn = re.sub(r'[^0-9]', '', time_h)
 
     time_m = time_dn + "_" + time_hn
+    time_m2 = time_dn + "_" +  str(int(time_hn[0:2]) + 12) + time_hn[2:4] + time_hn[4:6]
+
+    #print(time_m + ", " + time_m2)
 
     for img in img_files:
 
-        if time_m == os.path.basename(img)[0:-4]:
+        if (time_m == os.path.basename(img)[0:-4]) or (time_m2 == os.path.basename(img)[0:-4]):
+
+            #print(os.path.basename(img)[0:-4])
 
             ca_x, ca_y, ca_z = map(float, camera.split(", "))
             ma_x, ma_y, ma_z = map(float, marker.split(", "))
-            
+
             fix_x = ca_x - ma_x
             fix_y = ca_y - ma_y
             fix_z = ca_z - ma_z
 
+            distance = math.sqrt(fix_x ** 2 + fix_z ** 2)
 
+            destination = "./merged_img/" + str(round(distance, 2)) + "_" + os.path.basename(img)
+            shutil.copyfile(img, destination)
 
 
