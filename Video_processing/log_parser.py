4bdde4f1138c809d295d9017c3cb4050c7474dd0
JUNSUKSHIN <junsuk2017@chungbuk.ac.kr>
2022-12-07 09:23:43 +0900

    log parser

---
+++ Video_processing/log_parser.py
@@ -0,0 +1,39 @@
+import os
+
+f= open('position_log.txt','r')
+out = open('matrix_quotes.txt', 'w')
+lines = f.read().splitlines()
+
+last_string = "-1"
+
+tca_x, tca_y, tca_z = (0, 0, 0)
+tma_x, tma_y, tma_z = (0, 0, 0)
+
+cnt = 0
+
+
+for line in lines:
+    time_t, camera, marker = line.split("/")
+
+    if (last_string == "-1") or (last_string == time_t):
+
+        last_string = time_t
+
+        ca_x, ca_y, ca_z = map(float, camera[1:-1].split(", "))
+        ma_x, ma_y, ma_z = map(float, marker[1:-1].split(", "))
+
+        tca_x = tca_x + ca_x
+        tca_y = tca_y + ca_y
+        tca_z = tca_z + ca_z
+
+        tma_x = tma_x + ma_x
+        tma_y = tma_y + ma_y
+        tma_z = tma_z + ma_z
+
+        cnt = cnt + 1
+
+    else:
+        fca_x = tca_x / cnt
+        fca_y = tca_y / cnt
+        fca_z = tca_z / cnt
+
