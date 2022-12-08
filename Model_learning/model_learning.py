c8c722748bbdeb8f793b733e3cf0fa1823db607c
JUNSUKSHIN <junsuk2017@chungbuk.ac.kr>
2022-12-08 20:37:35 +0900

    load test dataset

--- Model_learning/model_learning.py
+++ Model_learning/model_learning.py
@@ -8,3 +8,9 @@ transforms_train = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
+
+transforms_test = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
