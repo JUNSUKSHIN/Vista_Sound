384e544131fea5ada665d4f322d71b317884a9f0
JUNSUKSHIN <junsuk2017@chungbuk.ac.kr>
2022-12-08 20:39:34 +0900

    load actual data from folders

--- Model_learning/model_learning.py
+++ Model_learning/model_learning.py
@@ -1,4 +1,5 @@
 import torch
+import os
 from torchvision import datasets, models, transforms
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -14,3 +15,10 @@ transforms_test = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
+
+data_dir = './custom_dataset'
+train_datasets = datasets.ImageFolder(os.path.join(data_dir, 'train'), transforms_train)
+test_datasets = datasets.ImageFolder(os.path.join(data_dir, 'test'), transforms_test)
+
+train_dataloader = torch.utils.data.DataLoader(train_datasets, batch_size=10, shuffle=True, num_workers=4)
+test_dataloader = torch.utils.data.DataLoader(test_datasets, batch_size=10, shuffle=True, num_workers=4)
