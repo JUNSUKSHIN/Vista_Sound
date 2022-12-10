05e13e87d42b96d4d66f4394e43c62c2404de596
JUNSUKSHIN <junsuk2017@chungbuk.ac.kr>
2022-12-10 13:14:54 +0900

    script for data classification

--- Video_processing/classification_by_distance.py
+++ Video_processing/classification_by_distance.py
@@ -17,14 +17,30 @@ for img in img_files:
         destination = img2_path + "0.5/" + os.path.basename(img)
         shutil.copyfile(img, destination)
 
-    if distance <= 0.75:
+    elif distance <= 0.75:
         destination = img2_path + "0.75/" + os.path.basename(img)
         shutil.copyfile(img, destination)
 
-    if distance <= 1:
+    elif distance <= 1:
         destination = img2_path + "1/" + os.path.basename(img)
         shutil.copyfile(img, destination)
 
-    if distance <= 1.25:
-        destination = img2_path + "0.75/" + os.path.basename(img)
+    elif distance <= 1.25:
+        destination = img2_path + "1.25/" + os.path.basename(img)
+        shutil.copyfile(img, destination)
+
+    elif distance <= 1.5:
+        destination = img2_path + "1.5/" + os.path.basename(img)
+        shutil.copyfile(img, destination)
+
+    elif distance <= 1.75:
+        destination = img2_path + "1.75/" + os.path.basename(img)
+        shutil.copyfile(img, destination)
+
+    elif distance <= 2:
+        destination = img2_path + "2/" + os.path.basename(img)
+        shutil.copyfile(img, destination)
+
+    else:
+        destination = img2_path + "2_over/" + os.path.basename(img)
         shutil.copyfile(img, destination)
