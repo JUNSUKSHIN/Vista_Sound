e4408a033da75fd05d39bc7b03c244329c72814a
JUNSUKSHIN <junsuk2017@chungbuk.ac.kr>
2022-12-10 12:54:51 +0900

    extract disntace from file name

--- Video_processing/classification_by_distance.py
+++ Video_processing/classification_by_distance.py
@@ -5,3 +5,11 @@ img_path = "./masked_img"
 img2_path = "./classified"
 img_files = glob.glob(img_path + '/*')
 img2_files = glob.glob(img2_path + '/*')
+
+for img in img_files:
+
+    print(os.path.basename(img)[16:-4])
+
+    distance = float(os.path.basename(img)[16:-4])
+
+    
